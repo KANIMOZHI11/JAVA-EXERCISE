47c7c19e6847c1c1257302c1bdafb0b6ffafb687
KANIMOZHI11 <50259668+KANIMOZHI11@users.noreply.github.com>
2019-05-04 15:28:54 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,10 @@
+public class Exercise1 {
+    public static void main(String [] args){
+    long x = 5;{
+    while ( x > 1 ) {
+      x = x - 1;
+      if ( x < 3) {
+        System.out.println("small x");
+      }
+    } } 
+}}
